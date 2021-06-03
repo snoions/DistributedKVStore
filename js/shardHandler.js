f4c4d3357c766e298bd806ff808ab06a362ebb85
Srilekha Vutukuru <vutukurusrilekha@gmail.com>
2021-06-02 22:22:34 -0700

    adding a new node works

--- js/shardHandler.js
+++ js/shardHandler.js
@@ -30,12 +30,16 @@ module.exports =  class ShardHandler{
 			else if (func == "shard-id-members")
 			    resJSON = await this.handleGetIdMembers(shard_id);
 			else if (func == "shard-id-key-count")
-			    resJSON = this.handleGetShardKeyCount(shard_id);
+			    resJSON = await this.handleGetShardKeyCount(shard_id);
 		}else if(method =="PUT"){
 		    if (func == "add-member")
-		        resJSON = this.handlePutMember(shard_id, data);
+		        resJSON = await this.handlePutMember(shard_id, data);
 		    else if (func == "reshard")
 		        resJSON = this.handlePutReshard(shard_id);
+		    else if (func == "set-shard")
+		        resJSON = this.handlePutNodeShardId(shard_id);
+		    else if (func == "set-shard-count")
+		        resJSON = this.handlePutShardCount(shard_id);
 		}
 		console.log("handled the request")
 		sendRes(resJSON);
@@ -91,32 +95,50 @@ module.exports =  class ShardHandler{
         return members;
     }
 
-    handleGetShardKeyCount(shard_id){
+    async handleGetShardKeyCount(shard_id){
         let resJSON = {}
         console.log("GET number of keys in a shard");
         resJSON['statusCode'] = 200
-        var count = Object.keys(this.storeHandler.kvstore).length;
         var nodesInShard = handleGetIdMembers(shard_id)['body'];
+        var tempView =[]
         for (var i =0; i<nodesInShard; i++){
-            if (nodesInShard[i]!= this.viewHandler.socket_address){
-                var url = "http://"+nodesInShard[i]+"/"+"key-value-store-all";
-                axios.get(url).then(res=>{
-                    var allKeys = res.data['kvstore']
-                    count+= Object.keys(allKeys).length
-                });
-            }
+            if (nodesInShard[i]!= this.viewHandler.socket_address)
+                tempView.push(nodesInShard[i])
         }
+        var count = await this.handleGetShardKeyCountHelper(shard_id, tempView);
         resJSON['body'] = {message:"Key count of shard ID retrieved successfully", "shard-id-key-count":count}
         return resJSON
     }
 
-	handlePutMember(shard_id, socket_address){
+    async handleGetShardKeyCountHelper(shard_id, nodesInShard){
+        var count = 0;
+        if (this.myShard == shard_id){
+            count += Object.keys(this.storeHandler.kvstore).length;
+        }
+        await Promise.all(nodesInShard.map(obj =>
+            axios.get("http://"+obj+"/"+"key-value-store-all").then(res=>{
+                var allKeys = res.data['kvstore']
+                count+= Object.keys(allKeys).length
+            })
+        ));
+        return count;
+    }
+
+	async handlePutMember(shard_id, socket_address){
 	    //send a request to the node at socket address
 	    //set myshard
 	    //set the shard count and shard ids -broadcast
-
 		let resJSON = {}
         console.log("PUT member in shard");
+        await axios.put("http://"+socket_address+"/key-value-store-shard/set-shard/"+shard_id, {}).then(res=>{
+            console.log(res.data.message);
+        })
+        if (shard_id > (this.shard_count-1)){
+            this.handlePutShardCount(shard_id);
+            this.viewHandler.broadcast("key-value-store-shard/set-shard-count/"+shard_id, "PUT", {}, (res) => {
+                console.log("broadcast of adding new member successful");
+            })
+        }
         resJSON['statusCode'] = 200
         return resJSON
 	}
@@ -141,8 +163,9 @@ module.exports =  class ShardHandler{
 	handlePutShardCount(shard_count){
 	    let resJSON = {}
         console.log("PUT shard count");
-        this.shard_count = shard_count
-        for (var i =0; i< shard_count; i++){
+        this.shard_count = shard_count+1;
+        this.shardIds = [];
+        for (var i =0; i<= shard_count; i++){
             this.shardIds.push(i);
         }
         resJSON['statusCode'] = 200
@@ -150,55 +173,6 @@ module.exports =  class ShardHandler{
         return resJSON
 	}
 
-    broadcast(endpoint, method, data, thenFunc){
-		const view_other = this.view.filter(address => address!=this.socket_address );  //view excluding the address of the current replica
-		console.log("in broadcast, view_other=", view_other)
-		for (let address of view_other) {
-		    this.sendAndDetectCrash(address, endpoint, method, data, thenFunc)
-		 }
-	}
-
-	async sequentialBroadcast(endpoint, method, data, thenFunc){
-		const view_other = this.view.filter(address => address!=this.socket_address );  //view excluding the address of the current replica
-		console.log("in broadcast, view_other=", view_other)
-		for (let address of view_other) {
-		    await this.sendAndDetectCrash(address, endpoint, method, data, thenFunc)
-		 }
-	}
-
-
-	async sendAndDetectCrash(address,endpoint, method, data, thenFunc){
-		let count = 1
-		let retry = true
-		 while(retry){
-			if(count>1)
-			 	await new Promise(r => setTimeout(r, 500));  //sleep half a second before retry
-			retry = false
-			let url = "http://"+address+"/"+endpoint
-			console.log("sending to "+url+ " attempt ", count)
-			await axios({url: url, method: method, data: data})
-		    .then (thenFunc)
-		    .catch((error) => {
-				if (error.response) {
-					console.log("Response error", {statusCode: error.response.status, data: error.response.data, url:  url});
-				} else if (error.request) {
-					// The request was made but no response was received
-					console.log("no response from ", address);
-					if (count==this.crash_threshold){
-						console.log(address+" crashed")
-						this.view = this.view.filter(a => a!=address ) 
-						this.broadcast('key-value-store-view', 'DELETE', {"socket-address": address})  //broadcast to delete the crashed node
-					}else{
-						retry = true;
-					 	count++;
-					}
-				}else {
-					console.log('axios error', error);
-				}
-				 
-			});
-		}
-	}
 
 	
 }
